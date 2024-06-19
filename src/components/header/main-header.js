b604de34abdf07296937ad65085dc81c41fb2d69
AGramont <alan.gramont@yahoo.com>
2024-06-18 22:24:00 -0500

    active link

--- src/components/header/main-header.js
+++ src/components/header/main-header.js
@@ -1,9 +1,19 @@
+"use client";
+
 import Link from "next/link";
 import RBALogo from "@/assets/rba-logo.webp";
 import Image from "next/image";
 import styles from "./main-header.module.css";
+import { usePathname } from "next/navigation";
 
 export default function MainHeader() {
+
+    const pathname = usePathname();
+    console.log("Pathname: ", pathname);
+
+    const links = [
+        { label: "People", href: "/people"}
+    ]
 
     return (
         <div className={styles.mainHeader}>
@@ -13,8 +23,18 @@ export default function MainHeader() {
             <div>
                 <nav className='app-link'>
                     <ul>
+                        {
+                            links.map(link => {
+                                console.log("link: ", link);
+                                const thisClassName = pathname.startsWith(link.href) ? "active-link" : "";
+                                console.log("thisClassName: ", thisClassName);
+                                return <li key={link.href}>
+                                    <Link href={link.href} className={thisClassName}>{link.label}</Link>
+                                </li>
+                            })
+                        }
                         <li>
-                            <Link href="/people">People</Link>
+                            
                         </li>
                     </ul>
                 </nav>
