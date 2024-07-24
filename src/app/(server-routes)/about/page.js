3c7385a37b945c1af68de4864e88d7cb4375d307
Alan Gramont <alan.gramont@yahoo.com>
2024-07-24 08:19:41 -0500

    about markers for dynamic options

--- src/app/(server-routes)/about/page.js
+++ src/app/(server-routes)/about/page.js
@@ -1,16 +1,27 @@
 import SubTitle from "@/components/format/sub-title";
+// import { headers } from "next/headers";
 
 export const metadata = {
   title: "About",
   description: "The about page",
 };
 
+// revalidated every n seconds
+//export const revalidate = 5;
+
+// force dynamic rendering
+//export const dynamic = "force-dynamic"
+
 export default function AboutPage() {
+  // accessing headers or cookies will also convert this to dynamic
+  // const myHeaders = headers();
+
   return (
     <>
       <SubTitle subTitle="About" />
       <div>
         <p>This is a static about page</p>
+        <p>My favorite number is: {Math.floor(Math.random() * 1000)}</p>
       </div>
     </>
   );
--- src/lib/db/peopleDB.js
+++ src/lib/db/peopleDB.js
@@ -1,28 +1,30 @@
-import 'server-only';
-import sql from 'better-sqlite3';
+import "server-only";
+import sql from "better-sqlite3";
 
-const db = sql('people.db');
-
-const delay = () => new Promise((r) => setTimeout(r, 1000));
+const db = sql("people.db");
+const delayValue = 10;
+const delay = () => new Promise((r) => setTimeout(r, delayValue));
 
 export async function getPeople() {
-    await delay();
-    return db.prepare(`select * from people where active = 1`).all();
+  await delay();
+  return db.prepare(`select * from people where active = 1`).all();
 }
 
 export async function getPerson(id) {
-    await delay();
-    return db.prepare("select * from people where id = ?").get(id);
+  await delay();
+  return db.prepare("select * from people where id = ?").get(id);
 }
 
 export async function deletePerson(personId) {
-    await delay();
-    return db.prepare("update people set active = 0 where id = ?").run(personId);
+  await delay();
+  return db.prepare("update people set active = 0 where id = ?").run(personId);
 }
 
 export async function createPerson(person) {
-    const value = {...person, active: 1}
-    const result = db.prepare(`
+  const value = { ...person, active: 1 };
+  const result = db
+    .prepare(
+      `
         INSERT INTO people VALUES (
             null,
             @firstname,
@@ -31,15 +33,18 @@ export async function createPerson(person) {
             @email,
             @phone,
             @active
-        )`).run(value);
-    const inserted = await getPerson(result.lastInsertRowid);
-    console.log("DB inserted: ", inserted);
-    return inserted;
+        )`
+    )
+    .run(value);
+  const inserted = await getPerson(result.lastInsertRowid);
+  console.log("DB inserted: ", inserted);
+  return inserted;
 }
 
 export async function updatePerson(person) {
-    console.log("db updating: ", person);
-    db.prepare(`
+  console.log("db updating: ", person);
+  db.prepare(
+    `
         UPDATE people SET 
         firstname = @firstname, 
         lastname = @lastname,
@@ -47,8 +52,9 @@ export async function updatePerson(person) {
         email = @email,
         phone = @phone,
         active = @active
-        WHERE id =@id`).run(person);
-    const result = await getPerson(person.id);
-    console.log("DB updated: ", result);
-    return result;
+        WHERE id =@id`
+  ).run(person);
+  const result = await getPerson(person.id);
+  console.log("DB updated: ", result);
+  return result;
 }
